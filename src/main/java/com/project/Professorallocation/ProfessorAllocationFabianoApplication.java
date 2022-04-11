cd436fe1887a4fa8d216554906ed619ceb0a476d
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-04-11 19:58:22 -0300

    ADD Entity classe

---
+++ src/main/java/com/project/Professorallocation/ProfessorAllocationFabianoApplication.java
@@ -0,0 +1,13 @@
+package com.project.Professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationFabianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationFabianoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/Professorallocation/model/Professor.java
@@ -0,0 +1,41 @@
+package com.project.Professorallocation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private long departmentid;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+		
+
+}

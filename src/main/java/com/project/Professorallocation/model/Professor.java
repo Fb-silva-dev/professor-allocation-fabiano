3af7b5bc83abf94a24e958e52a2583c234c3103f
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-04-11 22:23:54 -0300

    Add Entity class professor , allocation

--- src/main/java/com/project/Professorallocation/model/Professor.java
+++ src/main/java/com/project/Professorallocation/model/Professor.java
@@ -1,41 +1,82 @@
 package com.project.Professorallocation.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "professor")
 public class Professor {
 	@Id
+	@Column(nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(length = 100, nullable = false)
 	private String name;
+
+	@Column(length = 11, nullable = false, unique = true)
 	private String cpf;
-	private long departmentid;
-	
-	
+
+	@Column(name = "department_Id", nullable = false, unique = true)
+	private Long departmentId;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "department_Id", nullable = false, insertable = false, updatable = false)
+	private Department department;
+
+	public Professor() {
+		super();
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
-		
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + ", departmentId=" + departmentId + "]";
+	}
 
 }

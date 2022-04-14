b167a9387bba37adbe0f6f86d1178cab4fce0066
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-04-14 20:13:15 -0300

    Add Repository Course

---
+++ src/main/java/com/project/Professorallocation/repository/CourseRepository.java
@@ -0,0 +1,15 @@
+package com.project.Professorallocation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.Professorallocation.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+	 	
+		
+		
+	}
+
+

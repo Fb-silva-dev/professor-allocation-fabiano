91c6f8a9c802f3ed53c3edaae56a7df14ab6611c
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-04-16 16:47:01 -0300

    Add ProfessorRepo

--- src/main/java/com/project/Professorallocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/Professorallocation/repository/ProfessorRepository.java
@@ -1,10 +1,13 @@
 package com.project.Professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.Professorallocation.model.Professor;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+	List<Professor> findByDepartmentId(Long DepartmentId);
 
 }

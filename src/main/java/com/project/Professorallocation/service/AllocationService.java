57f4fb62ba53269d1f2598174cc7ab9cc759f2ab
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-05-07 15:18:21 -0300

    Ajust Allocation Service

--- src/main/java/com/project/Professorallocation/service/AllocationService.java
+++ src/main/java/com/project/Professorallocation/service/AllocationService.java
@@ -3,8 +3,6 @@ package com.project.Professorallocation.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import org.springframework.stereotype.Service;
 import com.project.Professorallocation.model.Allocation;
 import com.project.Professorallocation.repository.AllocationRepository;
 
@@ -26,7 +24,7 @@ public class AllocationService {
 		return repository.findAll();
 
 	}
-
+ 
 	public void deleteById(Long id) {
 		if (repository.existsById(id)) {
 			repository.deleteById(id);
@@ -36,16 +34,6 @@ public class AllocationService {
 	public Allocation create(Allocation allocation) {
 		allocation.setId(null);
 		return saveInternal(allocation);
-
-	}
-
-	private Allocation saveInternal(Allocation allocation) {
-		if (hasCollision(allocation)) {
-			throw new RuntimeException("There is a time collision for this allocation");
-
-		}
-		Allocation insertedAllocation = repository.save(allocation);
-		return insertedAllocation;
 	}
 
 	public Allocation update(Allocation allocation) {
@@ -58,27 +46,33 @@ public class AllocationService {
 		}
 	}
 
+	private Allocation saveInternal(Allocation allocation) {
+		if (hasCollision(allocation)) {
+			throw new RuntimeException("There is a time collision for this allocation");
+		}
+		allocation = repository.save(allocation);
+		return allocation;
+	}
+
 	private boolean hasCollision(Allocation newAllocation) {
-		List<Allocation> currentAllocations = repository.findByProfessorId(newAllocation.getprofessorId());
-		boolean collisionFound = false;
+		boolean hasCollision = false;
 
-		for (Allocation item : currentAllocations) {
-			if (hasCollision(item, newAllocation)) {
-				collisionFound = true;
+		List<Allocation> currentAllocations = AllocationRepository.findByProfessorId(newAllocation.getProfessorId());
+
+		for (Allocation currentAllocation : currentAllocations) {
+			hasCollision = hasCollision(currentAllocation, newAllocation);
+			if (hasCollision) {
 				break;
-
 			}
 		}
-		return collisionFound;
+ 
+		return hasCollision;
 	}
 
 	private boolean hasCollision(Allocation currentAllocation, Allocation newAllocation) {
-		boolean collision = !currentAllocation.getId().equals(newAllocation.getId())
-				&& currentAllocation.getProfessorId().equals(newAllocation.getProfessorId())
-				&& currentAllocation.getDayOfWeek().equals(newAllocation.getDayOfWeek())
+		return !currentAllocation.getId().equals(newAllocation.getId())
+				&& currentAllocation.getDayOfWeek() == newAllocation.getDayOfWeek() 
 				&& currentAllocation.getStartHour().compareTo(newAllocation.getEndHour()) < 0
 				&& newAllocation.getStartHour().compareTo(currentAllocation.getEndHour()) < 0;
-		return collision;
-
 	}
 }

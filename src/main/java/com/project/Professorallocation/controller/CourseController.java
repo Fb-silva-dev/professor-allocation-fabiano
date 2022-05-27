f04de00e1642d3b79a50aa422426f0130e8fef1c
Fabiano Silva <fb.tec.solva@hotmail.com>
2022-05-26 21:49:20 -0300

    Add Ajuste Controller Course

---
+++ src/main/java/com/project/Professorallocation/controller/CourseController.java
@@ -0,0 +1,57 @@
+package com.project.Professorallocation.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import com.project.Professorallocation.model.Course;
+import com.project.Professorallocation.service.CourseService;
+
+@RestController
+@RequestMapping(path = "/Cours") // vai definir qual o caminho do recurso.
+public class CourseController {
+
+	private CourseService service;
+
+	public CourseController(CourseService service) {
+		super();
+		this.service = service;
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Course>> findAll() {
+		List<Course> allCourses = service.findAll();
+
+		return new ResponseEntity<>(allCourses, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/{cours_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Course> findById(@PathVariable(name = "cours_id") Long id) {
+		Course item = service.findById(id);
+
+		if (item == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(item, HttpStatus.OK);
+		}
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Course> create(@RequestBody Course cours) {
+		Course item = service.Create(cours);
+
+		return new ResponseEntity<>(item, HttpStatus.CREATED);
+	}
+
+}
